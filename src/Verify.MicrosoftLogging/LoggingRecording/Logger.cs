9f8fa6823fb61d0d912e08650ea4d001d19faaca
Simon Cropp <simon.cropp@gmail.com>
2023-11-09 21:47:25 +1100

    .

--- src/Verify.MicrosoftLogging/LoggingRecording/Logger.cs
+++ src/Verify.MicrosoftLogging/LoggingRecording/Logger.cs
@@ -1,8 +1,24 @@
 ﻿class Logger(string? category) :
     ILogger
 {
-    public void Log<TState>(LogLevel level, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter) =>
-        LoggerProvider.AddEntry(level, category, eventId, state, exception, formatter);
+    public void Log<TState>(LogLevel level, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+    {
+        var message = formatter.Invoke(state, exception);
+        if (IsOriginalFormat(state))
+        {
+            var entry = new LogItem(level, category, eventId, exception, message, null);
+            Recording.Add("logs", entry);
+        }
+        else
+        {
+            var entry = new LogItem(level, category, eventId, exception, message, state);
+            Recording.Add("logs", entry);
+        }
+    }
+
+    static bool IsOriginalFormat<TState>(TState state) =>
+        state is IReadOnlyList<KeyValuePair<string, object>> {Count: 1} dictionary &&
+        dictionary.First().Key == "{OriginalFormat}";
 
     public bool IsEnabled(LogLevel level) =>
         true;
@@ -10,9 +26,12 @@
     public IDisposable BeginScope<TState>(TState state)
         where TState : notnull
     {
-        LoggerProvider.StartScope(state);
-        return new LoggerScope(LoggerProvider.EndScope);
+        Recording.Add("logs", new ScopeEntry("StartScope", state));
+        return new LoggerScope(EndScope);
     }
+
+    static void EndScope() =>
+        Recording.Add("logs", new ScopeEntry("EndScope", null));
 }
 
 class Logger<T>() :
--- src/Verify.MicrosoftLogging/LoggingRecording/LoggerRecording.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace VerifyTests;
-
-public static class LoggerRecording
-{
-    static AsyncLocal<LoggerProvider?> local = new();
-
-    public static LoggerProvider Start() =>
-        local.Value = new();
-}
